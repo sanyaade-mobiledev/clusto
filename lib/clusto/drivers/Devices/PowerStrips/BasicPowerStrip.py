07d3c829e7de18c9045371ef0431bc5bc430450b
ron <ron@54acbfd4-1f09-0410-b0c3-b2497be473bc>
2008-08-03 06:03:10 +0000

    add a common portmixin for devices that have ports
    git-svn-id: svn+ssh://mgmt.digg.internal/srv/svn/ops/clusto/trunk@1893 54acbfd4-1f09-0410-b0c3-b2497be473bc

--- lib/clusto/drivers/Devices/PowerStrips/BasicPowerStrip.py
+++ lib/clusto/drivers/Devices/PowerStrips/BasicPowerStrip.py
@@ -1,7 +1,8 @@
 
-from clusto.drivers.Base import Device
+from clusto.drivers.Base import Device, ResourceManagerMixin
+from clusto.drivers.common import PortMixin
 
-class BasicPowerStrip(Device):
+class BasicPowerStrip(PortMixin, ResourceManagerMixin, Device):
     """
     Basic power strip Driver.
     """
@@ -13,26 +14,3 @@ class BasicPowerStrip(Device):
     _properties = { 'maxport': 20,
                     'minport': 1 }
     
-    def checkType(self, portnumber):
-        """
-        Make sure that the portnumber given is valid and within the appropriate
-        range.
-        """
-
-        if (not isinstance(portnumber, int)
-            or portnumber < minport
-            or portnumber > maxport):
-            raise TypeError("portnumber must be an integer between %d and %d"
-                            % (self.minport, self.maxport))
-
-        return True
-
-    
-
-        
-    def connectDevice(self, device, port):
-        """
-        Connect a given device to a given port.
-        """
-
-        self.allocate(device, port)
--- lib/clusto/drivers/Devices/Switches/BasicNetworkSwitch.py
+++ lib/clusto/drivers/Devices/Switches/BasicNetworkSwitch.py
@@ -1,8 +1,8 @@
 
 from clusto.drivers.Base import Device, ResourceManagerMixin
+from clusto.drivers.common import PortMixin
 
-
-class BasicNetworkSwitch(ResourceManagerMixin, Device):
+class BasicNetworkSwitch(PortMixin, ResourceManagerMixin, Device):
     """
     Basic network switch driver
     """
@@ -13,26 +13,3 @@ class BasicNetworkSwitch(ResourceManagerMixin, Device):
     _properties = { 'maxport': 48,
                     'minport': 1 }
     
-    def checkType(self, portnumber):
-        """
-        Make sure that the portnumber given is valid and within the appropriate
-        range.
-        """
-
-        if (not isinstance(portnumber, int)
-            or portnumber < minport
-            or portnumber > maxport):
-            raise TypeError("portnumber must be an integer between %d and %d"
-                            % (self.minport, self.maxport))
-
-        return True
-
-    
-
-        
-    def connectDevice(self, device, port):
-        """
-        Connect a given device to a given port.
-        """
-
-        self.allocate(device, port)
