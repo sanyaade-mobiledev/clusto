390e3ab25c20ba343519598fffd08097cc135442
ron <ron@54acbfd4-1f09-0410-b0c3-b2497be473bc>
2008-09-24 11:41:03 +0000

    * IPmanger cleanup * still working on allocator
    git-svn-id: svn+ssh://mgmt.digg.internal/srv/svn/ops/clusto/trunk@2253 54acbfd4-1f09-0410-b0c3-b2497be473bc

--- lib/clusto/drivers/resources/ipmanager.py
+++ lib/clusto/drivers/resources/ipmanager.py
@@ -21,30 +21,48 @@ class IPManager(ResourceManager):
     def ipy(self):
 	if not hasattr(self, '__ipy'):
 
-	    self.__ipy = IPy.IP(''.join([self.baseip, '/', self.netmask]))
+	    self.__ipy = IPy.IP(''.join([str(self.baseip), '/', self.netmask]),
+				make_net=True)
 
 
 	return self.__ipy
 
-    def checkType(self, resource):
+    def ensureType(self, resource, numbered=True, subkey=None):
+
 	"""check that the given ip falls within the range managed by this manager"""
 
 	try:
-	    ip = IPy.IP(resource)
+	    if not isinstance(numbered, bool) and isinstance(numbered, int):
+		ip = IPy.IP(numbered)
+	    else:
+		ip = IPy.IP(resource)
 	except ValueError:
 	    raise ResourceTypeException("%s is not a valid ip."
 					% resource)
 
-	if self.baseip == None:
-	    return ip.strDec()
+	if self.baseip and (ip not in self.ipy):
+	    raise ResourceTypeException("The ip %s is out of range for this IP manager.  Should be in %s/%s"
+					% (str(resource), self.baseip, self.netmask))
 
-	else:
 
-	    if ip not in self.ipy:
-		raise ResourceTypeException("The ip %s is out of range for this IP manager.  Should be in %s/%s"
-					    % (str(resource), self.baseip, self.netmask))
-	    else:
-		return ip.strDec()
+	return ('ip', ip.int(), None)
+
+
+    def allocator(self):
+	"""allocate IPs from this manager"""
+	
+	if self.baseip is None:
+	    raise ResourceTypeException("Cannot generate an IP for an ipManager with no baseip")
+
+	startip=self.ipy.net().int() + 1
+
+	if not self.attrs('ip', numbered=startip):
+	    return ('ip', startip, None)
+
+	ipcounter = 1 # we already know the first address is used up
+	iplen = self.ipy.len() # so I know when I've tried all IPs
+	workinip = startip
+
+	raise NotImplemented("Still working on allocating free IPs")
 
 	    
-	
