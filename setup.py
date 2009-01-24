ac739abd2f94c2a4d1037bb8862824fae95e5b9c
ron <ron@54acbfd4-1f09-0410-b0c3-b2497be473bc>
2009-01-24 11:09:53 +0000

    move scripts to src directory
    git-svn-id: svn+ssh://mgmt.digg.internal/srv/svn/ops/clusto/trunk@3028 54acbfd4-1f09-0410-b0c3-b2497be473bc

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ import sys
 
 from setuptools import setup, find_packages
 
-srcdir = os.path.dirname(sys.argv[0])
+srcdir = os.path.join(os.path.dirname(sys.argv[0]), 'src')
 
 setup(
     name = "Clusto",
-    version = "0.1.3",
+    version = "0.1.6",
     packages = find_packages('src'),
     author = "Ron Gorodetzky",
     author_email = "ron@digg.com",
     description = "Clusto, cluster management and inventory system",
-    install_requires = ['sqlalchemy>=0.5.0beta2', 'IPy>=0.55', 'IPython'],
+    install_requires = ['sqlalchemy>=0.5.0', 'IPy>=0.55', 'IPython'],
     package_dir = {'':'src'},
     #scripts=glob.glob(os.path.join(srcdir, 'scripts', 'clusto')),
     scripts=[os.path.join(srcdir, 'scripts', 'clusto'), 
