2dbe735d8446a1cca456cd0654f931e80cd785ef
ron <ron@54acbfd4-1f09-0410-b0c3-b2497be473bc>
2009-01-27 01:35:42 +0000

    added simplenummanager class
    git-svn-id: svn+ssh://mgmt.digg.internal/srv/svn/ops/clusto/trunk@3067 54acbfd4-1f09-0410-b0c3-b2497be473bc

---
+++ src/clusto/drivers/resources/simplenummanager.py
@@ -0,0 +1,49 @@
+import clusto
+from clusto.drivers.base import ResourceManager
+
+from clusto.exceptions import ResourceException
+from clusto.schema import ATTR_TABLE
+
+class SimpleNumManagerException(ResourceException):
+    pass
+
+class SimpleNumManager(ResourceManager):
+    """Manage the generation of numbers that can be associated with Entities
+    
+    """
+
+    _driverName = "simplenummanager"
+    _properties = {'maxnum':None,
+                   'next':None,
+		   }
+
+    _recordAllocations = True
+    
+    def __init__(self, nameDriverEntity,
+                 startingnum=0,
+		 maxnum=False,
+                 *args, **kwargs):
+
+
+        super(ResourceManager, self).__init__(nameDriverEntity,
+                                              *args, **kwargs)
+        
+	if maxnum:
+	    self.maxnum = maxnum
+
+	self.next = startingnum
+
+
+    def allocator(self):
+
+	clusto.flush()
+        num = self.next
+	
+        if self.maxnum and num > self.maxnum:
+            raise SimpleNumManagerException("Out of numbers. "
+					    "Max of %d reached." 
+					    % (self.maxnum))
+        
+        self.next = ATTR_TABLE.c.int_value + 1
+
+        return ('num', num)
---
+++ src/clusto/test/drivers/resourcemanagers/simplenummanagertest.py
@@ -0,0 +1,54 @@
+import clusto
+from clusto.test import testbase 
+import itertools
+
+from clusto.drivers import *
+
+from clusto.drivers.resources.simplenummanager import *
+
+
+
+class SimpleNumManagerTests(testbase.ClustoTestBase):
+
+    def data(self):
+
+        n1 = SimpleNumManager('numgen1', startingnum=1)
+
+
+        n2 = SimpleNumManager('numgen2', maxnum=4, startingnum=0)
+        
+        clusto.flush()
+
+    def testAllocateNum(self):
+
+        ngen = clusto.getByName('numgen1')
+        
+	d = Driver('foo')
+        s1 = ngen.allocate(d)
+        s2 = ngen.allocate(d)
+        s3 = ngen.allocate(d)
+        s4 = ngen.allocate(d)
+
+	self.assertEqual(ngen.owners('num', 1), [d])
+	self.assertEqual(ngen.owners('num', 2), [d])
+	self.assertEqual(ngen.owners('num', 3), [d])
+	self.assertEqual(ngen.owners('num', 4), [d])
+
+	self.assertEqual(sorted(ngen.resources(d)),
+			 sorted(ngen.attrs(key='resource')))
+
+    def testAllocateMaxNum(self):
+	
+	d = Driver('foo')
+
+	ngen = clusto.getByName('numgen2')
+
+	s1 = ngen.allocate(d)
+	s1 = ngen.allocate(d)
+	s1 = ngen.allocate(d)
+	s1 = ngen.allocate(d)
+	s1 = ngen.allocate(d)
+
+	self.assertRaises(SimpleNumManagerException, ngen.allocate, d)
+	
+	
