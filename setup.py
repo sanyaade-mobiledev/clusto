028dce00c5378132628bbfdaae0766f4267cadbc
ron <ron@54acbfd4-1f09-0410-b0c3-b2497be473bc>
2009-02-11 11:09:01 +0000

    * increments version
    git-svn-id: svn+ssh://mgmt.digg.internal/srv/svn/ops/clusto/trunk@3355 54acbfd4-1f09-0410-b0c3-b2497be473bc

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ srcdir = os.path.join(os.path.dirname(sys.argv[0]), 'src')
 
 setup(
     name = "clusto",
-    version = "0.1.13",
+    version = "0.1.14",
     packages = find_packages('src'),
     author = "Ron Gorodetzky",
     author_email = "ron@digg.com",
