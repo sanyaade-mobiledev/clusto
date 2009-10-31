0ed743ab2d5c0a652e69ae2c3b48ef0cbe1fae49
Ron Gorodetzky <ron@digg.com>
2009-10-30 21:43:51 -0700

    add versioning tests for viewing old versions of Entities and Attributes

--- src/clusto/test/base/versioningtests.py
+++ src/clusto/test/base/versioningtests.py
@@ -54,3 +54,56 @@ class TestClustoVersioning(testbase.ClustoTestBase):
         self.assertEqual(etest.entity.version, curver+3)
 
 
+    def testViewOldVersion(self):
+
+        curver = clusto.get_latest_version_number()
+
+        e1 = Entity('e1')
+        e2 = Entity('e2')
+        e3 = Entity('e3')
+
+        self.assertEqual(Entity.query().filter(Entity.name.like('e%')).count(),
+                         3)
+
+        SESSION.version = curver
+
+        self.assertEqual(Entity.query().filter(Entity.name.like('e%')).count(),
+                         0)
+
+        SESSION.version = clusto.working_version()
+
+        self.assertEqual(Entity.query().filter(Entity.name.like('e%')).count(),
+                         3)
+
+        SESSION.version = curver + 1
+
+        self.assertEqual(Entity.query().filter(Entity.name.like('e%')).count(),
+                         1)
+
+
+        SESSION.version = curver + 2
+
+        self.assertEqual(sorted([e1,e2]),
+                         Entity.query().filter(Entity.name.like('e%')).all())
+
+    def testOldVersionsOfAttributes(self):
+
+        curver = clusto.get_latest_version_number()
+
+        e1 = Entity('e1')
+        e2 = Entity('e2')
+
+        e1.add_attr('foo', 1)
+        e1.add_attr('foo2', 2)
+        e1.add_attr('foo3', 3)
+
+        SESSION.version = curver + 3
+
+        self.assertEqual(len(list(e1.attrs)), 1)
+        
+        e = Entity.query().filter_by(name='e1').one()
+
+        SESSION.version = curver + 4
+
+        self.assertEqual(sorted([a.key for a in e.attrs]),
+                         sorted(['foo', 'foo2']))
