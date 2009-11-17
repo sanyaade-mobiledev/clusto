6c803f5118e87ce49372f622f4fff044dc3a3b37
Jeremy Grosser <synack@digg.com>
2009-11-17 13:49:53 -0800

    Use scapy for SNMP stuff

--- src/clusto/drivers/devices/powerstrips/servertech.py
+++ src/clusto/drivers/devices/powerstrips/servertech.py
@@ -79,58 +79,39 @@ class PowerTowerXM(BasicPowerStrip, IPMixin, SNMPMixin):
 
         return num
 
-    def _create_snmp_session(self):
-        ip = IPManager.get_ips(self)
-        if not ip:
-            raise DriverException('Power switch %s has no IP address' % self.name)
-        ip = ip[0]
-
-        community = self.attr_values(key='snmp', subkey='community', merge_container_attrs=True)
-        if not community:
-            raise DriverException('Power switch %s has no key=snmp,subkey=community attribute' % self.name)
-        community = community[0]
-
-        return netsnmp.Session(Version=2, DestHost=ip, Community=community)
-
     def _get_port_oid(self, outlet):
         for oid, value in self._snmp_walk('1.3.6.1.4.1.1718.3.2.3.1.2'):
-            if value.val.lower() == outlet.lower.lstrip('.'):
+            if value.lower() == outlet:
                 return oid
 
     def get_outlet_state(self, outlet):
-        index = self._get_port_oid(outlet).split('1.3.6.1.4.1.1718.3.2.3.1.2', 1)
-        oid = '1.3.6.1.4.1.1718.3.2.3.1.10.%s' % index
-        state = session.get(netsnmp.VarList(netsnmp.Varbind(oid)))
-        state = state[0]
-        if state == None:
-            raise DriverException('Outlet %s does not exist' % outlet)
+        oid = self._get_port_oid(outlet)
+        oid = oid.replace('1.3.6.1.4.1.1718.3.2.3.1.2', '1.3.6.1.4.1.1718.3.2.3.1.10')
+        state = self._snmp_get(oid)
         return self._outlet_states[int(state)]
 
     def set_outlet_state(self, outlet, state, session=None):
-        if not session:
-            session = self._create_snmp_session()
-        index = self._get_port_oid_index(outlet, session=session)
-        oid = '.1.3.6.1.4.1.1718.3.2.3.1.11.%s' % index
-        vars = netsnmp.VarList(netsnmp.Varbind(oid, '', state, 'INTEGER'))
-        if session.set(vars) != 1:
-            raise DriverException('Failed to set SNMP state %i for OID %s' % (state, oid))
-            
+        oid = self._get_port_oid(outlet)
+        oid = oid.replace('1.3.6.1.4.1.1718.3.2.3.1.2', '1.3.6.1.4.1.1718.3.2.3.1.11')
+        r = self._snmp_set(oid, state)
+        if r.PDU.varbindlist[0].value.val != state:
+            raise DriverException('Unable to set SNMP state')
+
     def reboot(self, porttype, portnum):
         if porttype != 'pwr-nema-5':
             raise DriverException('Cannot reboot ports of type: %s' % str(porttype))
 
         portnum = portnum.lstrip('.').lower()
 
-        session = self._create_snmp_session()
-        state = self.get_outlet_state(portnum, session=session)
+        state = self.get_outlet_state(portnum)
 
         nextstate = None
         if state == 'off':
             nextstate = 1
-        if state in ('idleOn', 'on'):
+        if state in ('idleOn', 'on', 'wakeOn'):
             nextstate = 3
 
         if not nextstate:
             raise DriverException('Outlet in unexpected state: %s' % state)
 
-        self.set_outlet_state(portnum, nextstate, session=session)
+        self.set_outlet_state(portnum, nextstate)
