da88ec991f04077fcfa70405fc0f85b837eae3d9
Jeremy Grosser <synack@digg.com>
2010-03-25 17:03:57 -0700

    Many bugfixes for watch_snmp, rackfactory for handling rack layouts

--- contrib/watch_snmp.py
+++ contrib/watch_snmp.py
@@ -1,77 +1,77 @@
-#!/home/synack/src/clusto/env/bin/python
+#!/usr/bin/env python
 from socket import socket, AF_INET, SOCK_DGRAM
 from traceback import format_exc
 from time import strftime, time, localtime, sleep
 from struct import unpack
+import sys
 
 import logging
+
+log = logging.getLogger('clusto.snmp')
+fmt = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s', '%Y-%m-%d %H:%M:%S')
+handler = logging.FileHandler('/var/log/clusto.log')
+handler.setFormatter(fmt)
+log.addHandler(handler)
+log.setLevel(logging.INFO)
+
 runtime = logging.getLogger('scapy.runtime')
 runtime.setLevel(logging.ERROR)
+
 from scapy.all import SNMP
 
 from clusto.scripthelpers import init_script
 from clusto.drivers import IPManager, PenguinServer
 import clusto
 
-from discovery import SWITCHPORT_TO_RU, RU_TO_PWRPORT
+import rackfactory
 
 def update_clusto(trap):
     ts = strftime('[%Y-%m-%d %H:%M:%S]')
     if trap['operation'] != 1:
         return
 
-    switch = IPManager.get_device(trap['switch'])
+    switch = IPManager.get_devices(trap['switch'])
     if not switch:
-        print ts, "Trap received from a device that clusto doesn't know about", trap['switch']
+        log.warning('Unknown trap source: %s' % trap['switch'])
         return
     else:
         switch = switch[0]
-        #print 'Got trap from', switch.name
-
-    if trap['port'] > 20:
-        return
 
     if not switch.attrs(key='snmp', subkey='discovery', value=1, merge_container_attrs=True):
-        print 'Discovery disabled for', switch.name
+        log.debug('key=snmp, subkey=discovery for %s not set to 1, ignoring trap' % switch.name)
         return
 
     server = switch.get_connected('nic-eth', trap['port'])
     if not server:
         servernames = clusto.get_by_name('servernames')
-        print ts, 'Allocating new server on', trap['switch'], trap['port']
+        clusto.SESSION.clusto_description = 'SNMP allocate new server'
         server = servernames.allocate(PenguinServer)
+        log.info('Created new server on %s port %s: %s' % (trap['switch'], trap['port'], server.name))
 
-    print ts, server.name, repr(trap)
+    rack = switch.parents(clusto_types=['rack'])[0]
 
-    ru = SWITCHPORT_TO_RU[trap['port']]
+    try:
+        factory = rackfactory.get_factory(rack.name)
+    except:
+        log.error(format_exc())
+        return
 
-    ifnum = int(trap['port'] > 21) + 1
+    try:
+        clusto.begin_transaction()
+        if not trap['mac'] in server.attr_values(key='bootstrap', subkey='mac', value=trap['mac']):
+            log.debug('Adding bootstrap mac to', server.name)
+            server.add_attr(key='bootstrap', subkey='mac', value=trap['mac'])
 
-    clusto.begin_transaction()
-    if not trap['mac'] in server.attr_values(key='bootstrap', subkey='mac', value=trap['mac']):
-        print 'Adding bootstrap mac to', server.name
-        server.add_attr(key='bootstrap', subkey='mac', value=trap['mac'])
+        factory.add_server(server, trap['port'])
+        switch.set_port_attr('nic-eth', trap['port'], 'vlan', trap['vlan'])
 
-    if server.port_free('nic-eth', ifnum):
-        server.connect_ports('nic-eth', ifnum, switch, trap['port'])
-    switch.set_port_attr('nic-eth', trap['port'], 'vlan', trap['vlan'])
+        clusto.SESSION.clusto_description = 'SNMP update MAC and connections on %s' % server.name
+        clusto.commit()
+    except:
+        log.error(format_exc())
+        clusto.rollback_transaction()
 
-    if server.port_free('pwr-nema-5', 1):
-        power = switch.port_info['pwr-nema-5'][1]['connection']
-        server.connect_ports('pwr-nema-5', 1, power, RU_TO_PWRPORT[ru])
-
-    if server.port_free('console-serial', 1):
-        ts = switch.port_info['console-serial'][1]['connection']
-        server.connect_ports('console-serial', 1, ts, trap['port'])
-
-    rack = [x for x in switch.parents() if x.type == 'rack']
-    if rack:
-        rack = rack[0]
-        if not server in rack:
-            rack.insert(server, ru)
-    clusto.commit()
-
-    print ts, switch.name, server.name, trap['mac']
+    log.debug(repr(trap))
 
 def trap_listen():
     sock = socket(AF_INET, SOCK_DGRAM)
@@ -87,7 +87,7 @@ def trap_listen():
                 result = {
                     'operation': ord(operation),
                     'vlan': vlan,
-                    'mac': ':'.join([('%x' % ord(x)).ljust(2, '0') for x in mac]).lower(),
+                    'mac': ':'.join([('%x' % ord(x)).rjust(2, '0') for x in mac]).lower(),
                     'port': port,
                     'switch': address[0],
                 }
@@ -98,9 +98,10 @@ def main():
         try:
             update_clusto(trap)
         except:
-            print 'Exception in run_snmp process'
-            print format_exc()
+            log.error('Exception in run_snmp process')
+            log.error(format_exc())
 
 if __name__ == '__main__':
+    log.info('Clusto SNMP server starting')
     init_script()
-    main()
+    sys.exit(main())
